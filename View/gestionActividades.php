b70185242d34e7fc591bb6a59b4abfedaa0cc24c
alexvaloc <alejandravalenciaocampo@gmail.com>
2024-03-29 15:27:02 +0100

    actividades ready

--- View/gestionActividades.php
+++ View/gestionActividades.php
@@ -42,22 +42,26 @@ $totalPrecios = $actividadController->calcularTotalPrecioPorDestinoController($i
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/btn.css">
-    <link rel="stylesheet" href="../assets/css/gestionActividades.css">
      <!--BOOTSTRAP CSS-->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!--FontAwesome-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!--CSS general-->
+    <link rel="stylesheet" href="../assets/css/btn.css">
+    <!--CSS Específico de página-->
+    <link rel="stylesheet" href="../assets/css/gestionActividades.css">
     <title>Gestión de actividades - Mytra</title>
 </head>
 <body style="background: #ececec;">
+
 <?php include './navbar.php'; ?>
-<!-- mensaje de exito o error al crear/editar/eliminar -->
-<?php if (isset($_SESSION['mensaje'])) {
-        echo "<p>" . $_SESSION['mensaje'] . "</p>";
-        unset($_SESSION['mensaje']); // Borra el mensaje después de mostrarlo
-    }
-    ?>
+
+    <!--mensaje exito/error-->
+        <?php if (isset($_SESSION['mensaje'])): ?>
+            <div class="alert alert-info text-center"><?php echo $_SESSION['mensaje']; ?></div>
+            <?php unset($_SESSION['mensaje']); // Elimina el mensaje de la sesión después de mostrarlo ?>
+        <?php endif; ?>
+
 <div class="container">
     <!--Mostramos la información del viaje actual-->
     <div class="row mb-3 informacion-viaje mb-0 mt-4">
@@ -74,14 +78,12 @@ $totalPrecios = $actividadController->calcularTotalPrecioPorDestinoController($i
         </div>
     </div> 
 
-<div class="informacion-destino">
-      
-</div>
+
 <br><hr>
 
-    <div class="row mb-3">
+    <div class="row mb-3" style="background: #376d6f;">
         <div class="col text-center">
-            <h2 class="display-5 mt-4 mb-2">Actividades planeadas</h2>
+            <h2 class="display-5 mt-2 mb-2" style="color: #f5f5f5;">Actividades planeadas</h2>
         </div>
     </div>
 <!--Botón para añadir nuevas actividades-->
@@ -123,15 +125,16 @@ $totalPrecios = $actividadController->calcularTotalPrecioPorDestinoController($i
     </form>
     </div>
 </div>
-    <table class="tabla-actividades">
-        <thead>
+<div class="container" style="background: #16b3a623;">
+    <table class="table table-striped table-hover table-bordered mt-3 tabla-actividades">
+        <thead class="thead-dark text-center" id="cabecera" >
             <tr>
                 <th>Nombre</th>
                 <th>Descripción</th>
                 <th>Fecha</th>
                 <th>Duración</th>
                 <th>Precio</th>
-                <th></th>
+                <th style="min-width: 70px;"></th> <!--Ancho mínimo de columna para evitar que se superpongan los botones-->
             </tr>
         </thead>
         <tbody>
@@ -144,7 +147,7 @@ $totalPrecios = $actividadController->calcularTotalPrecioPorDestinoController($i
                     <td><?php echo htmlspecialchars($actividad['Precio'])?>€</td>
                     <td>
                     <!-- Botones de editar y eliminar -->
-                    <button class="btn-editar-act"
+                    <button class="btn-editar-act btn-editar mb-2"
                     data-id-actividad="<?= $actividad['id_actividad'] ?>"
                     data-nombre="<?= htmlspecialchars($actividad['nombre_actividad'])?>"
                     data-descripcion="<?= htmlspecialchars($actividad['descripcion'])?>"
@@ -153,46 +156,70 @@ $totalPrecios = $actividadController->calcularTotalPrecioPorDestinoController($i
                     data-precio="<?=htmlspecialchars($actividad['Precio']) ?>" >
                     <i class="fas fa-edit"></i>
                     </button>
-                    <a href="../Controller/EliminarActividad.php?id_actividad=<?= $actividad['id_actividad'] ?>&id_destino=<?= $idDestino?>" class="btn-eliminar-act" onclick="return confirm('¿Estás seguro de querer eliminar esta actividad?');">
+                    <a href="../Controller/EliminarActividad.php?id_actividad=<?= $actividad['id_actividad'] ?>&id_destino=<?= $idDestino?>" class="btn-eliminar-act btn-eliminar" onclick="return confirm('¿Estás seguro de querer eliminar esta actividad?');">
                         <i class="fas fa-trash-alt"></i>
                     </a>
                 </td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
-        <!--Formulario de edición de actividades-->
-        <div id="formularioEditarActividad" style="display:none;">
-            <form  method="POST" action="../Controller/ActualizarActividad.php">
+        
+    </table>
+
+    <!--Formulario de edición de actividades-->
+    
+        <div class="container">
+        <div id="formularioEditarActividad" class="container mt-3" style="display:none;">
+            <form  method="POST" class="row g-3" action="../Controller/ActualizarActividad.php">
                 <input type="hidden" name="id_actividad" value="<?php echo htmlspecialchars($actividad['id_actividad']);?>">
                 <input type="hidden" name="id_destino" value="<?php echo $idDestino; ?>">
 
-                <label for="nombre_actividad">Nombre de la actividad:</label>
-                <input type="text" id="nombre_actividad" name="nombre_actividad" value="<?php echo htmlspecialchars($actividad['nombre_actividad']);?>" required>
+                <div class="col-md-12 my-2">
+                <label for="nombre_actividad" class="form-label">Nombre de la actividad:</label>
+                <input type="text" class="form-control" id="nombre_actividad" name="nombre_actividad" value="<?php echo htmlspecialchars($actividad['nombre_actividad']);?>" required>
+                </div>
 
-                <label for="descripcion">Descripción:</label>
-                <textarea name="descripcion" id="descripcion" rows="4"><?php echo htmlspecialchars($actividad['descripcion']);?></textarea>
+                <div class="col-md-6">
+                <label for="fecha" class="form-label">Fecha:</label>
+                <input type="date" class="form-control" id="fecha" name="fecha" value="<?php echo htmlspecialchars($actividad['fecha']);?>"required>
+                </div>
 
-                <label for="fecha">Fecha:</label>
-                <input type="date" id="fecha" name="fecha" value="<?php echo htmlspecialchars($actividad['fecha']);?>"required>
+                <div class="col-md-6">
+                <label for="duracion" class="form-label">Duración (HH:MM):</label>
+                <input type="time" class="form-control" id="duracion" name="duracion" value="<?php echo htmlspecialchars($actividad['duracion']);?>" required>
+                </div>
 
-                <label for="duracion">Duración (HH:MM):</label>
-                <input type="time" id="duracion" name="duracion" value="<?php echo htmlspecialchars($actividad['duracion']);?>" required>
+                <div class="col-md-6">
+                <label for="Precio" class="form-label">Precio (€):</label>
+                <input type="number" class="form-control" id="Precio" name="Precio" step="0.01" value="<?php echo htmlspecialchars($actividad['Precio']);?>" required>
+                </div>
 
-                <label for="Precio">Precio (€):</label>
-                <input type="number" id="Precio" name="Precio" step="0.01" value="<?php echo htmlspecialchars($actividad['Precio']);?>" required>
+                <div class="col-md-6">
+                <label for="descripcion" class="form-label">Descripción:</label>
+                <textarea name="descripcion" class="form-control" id="descripcion" rows="4"><?php echo htmlspecialchars($actividad['descripcion']);?></textarea>
+                </div>
 
-                <button type="submit" class="btn-crear-actividad">Guardar cambios</button>   
-                <button type="button" id="btnCerrarFormularioAct">Cerrar</button>
+                <div class="col-10">
+                <button type="submit" class="btn-primary mb-4">Guardar cambios</button>   
+                </div>
+                <div class="col-2">
+                <button type="button" class="btn-editar" id="btnCerrarFormularioAct">Cerrar</button>
+                </div>
             </form>
-
         </div>
-    </table>
+        </div>
+        
+        
+</div>
 
-    <div class="total-precios">
-        <h3>Total de Precios de Actividades: €<?php echo htmlspecialchars($totalPrecios); ?></h3>
-    </div>
-    <div>
-        <a href="./gestionViaje.php?id_viaje=<?= $idViaje ?>&id_destino=<?= $idDestino ?>">Volver a mis destinos</a>
+    <div class="container total-precios my-4 py-3">
+        <h3 class="total-precio-texto text-center">Total de Precios de Actividades: 
+            <span class="precio-cifra">€<?php echo htmlspecialchars($totalPrecios); ?></span>
+        </h3>
+    
+        <div class="text-center">
+            <a href="./gestionViaje.php?id_viaje=<?= $idViaje ?>&id_destino=<?= $idDestino ?>" class="input-tablero">Volver a mis destinos</a>
+        </div>
     </div>
 
 </div>
